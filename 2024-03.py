2f85a9749919a5a92738b35c9aab4ed55389e46e
Alex Terehov <terales@users.noreply.github.com>
2024-12-03 04:42:52 -0330

    d3c2: solved

--- 2024-03.py
+++ 2024-03.py
@@ -12,6 +12,23 @@ def main(memory):
 
   return output
 
+def conditionalMain(memory):
+  conditionalsRegex = re.compile("(do\(\)|don't\(\))")
+  conditionals = re.split(conditionalsRegex, memory)
+
+  output = 0
+  multiplicationsEnabled = True
+
+  for step in conditionals:
+    if step == 'do()':
+      multiplicationsEnabled = True
+    elif step == "don't()":
+      multiplicationsEnabled = False
+    elif multiplicationsEnabled:
+      output += main(step)
+
+  return output
+
 memorySample = '''
 xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
 '''.strip()
@@ -19,5 +36,11 @@ xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
 with open('2024-03-input.txt') as f:
     memoryFirstChallenge = f.read()
 
+memorySecondSample = '''
+xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
+'''.strip()
+
 print("Sample:", main(memorySample))
 print("First challenge:", main(memoryFirstChallenge))
+print("Second sample:", conditionalMain(memorySecondSample))
+print("Second challenge:", conditionalMain(memoryFirstChallenge))
