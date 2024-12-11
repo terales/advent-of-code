838ceee0a764ac422ea24829290dcf8f0261ae8c
Alex Terehov <terales@users.noreply.github.com>
2024-12-11 09:49:14 -0330

    d11c2: solved by rewriting 😎

--- 2024-11.py
+++ 2024-11.py
@@ -1,3 +1,4 @@
+from functools import cache
 from yaml import load, SafeLoader
 
 UNCHANGED_STONE_MULTIPLIER = 2024
@@ -6,32 +7,35 @@ def buildStones(input):
   return list(map(int, input.split(' ')))
 
 def main(input, blinks):
-  stones = buildStones(input)
-  changedStones = blink(stones, blinks)
-  return len(changedStones)
+  stones = list(map(int, input.split(' ')))
+  stonesCount = 0
+  for stone in stones:
+    stonesCount += countStonesAfterBlink(stone, blinks)
+  return stonesCount
 
-def blink(stones, blinks):
-  currentStones = stones
-  for blink in reversed(range(1, blinks + 1)):
-    changedStones = []
-    for stone in currentStones:
-      if stone == 0:
-        changedStones.append(1)
-        continue
-      
-      digits = len('%i' % stone)
-      if not digits & 1:
-        # Fastest way to get number of digits in the number https://stackoverflow.com/a/54054183
-        # Algorithmic way to split number by digits https://www.reddit.com/r/learnpython/comments/10iw55k/comment/j5ien9m
-        halfLen = digits // 2
-        halfLenDivider = 10 ** halfLen
-        changedStones.append(stone // halfLenDivider)
-        changedStones.append(stone % halfLenDivider)
-        continue
+@cache
+def countStonesAfterBlink(stone, blinks):
+  blinksLeft = blinks - 1
+  if blinks <= 0:
+    return 1
 
-      changedStones.append(stone * UNCHANGED_STONE_MULTIPLIER)
-    currentStones = changedStones
-  return currentStones
+  if stone == 0:
+    return countStonesAfterBlink(1, blinksLeft)
+    
+  digits = len('%i' % stone) # Fastest way to get number of digits in the number https://stackoverflow.com/a/54054183
+  if digits % 2 == 0:
+      # Algorithmic way to split number by digits https://www.reddit.com/r/learnpython/comments/10iw55k/comment/j5ien9m
+      halfLen = digits // 2
+      halfLenDivider = 10 ** halfLen
+      leftStones = countStonesAfterBlink(stone // halfLenDivider, blinksLeft)
+      rightStones = countStonesAfterBlink(stone % halfLenDivider, blinksLeft)
+      return leftStones + rightStones
+
+  return countStonesAfterBlink(stone * UNCHANGED_STONE_MULTIPLIER, blinksLeft)
+
+
+with open('2024-11-samples.yaml') as f:
+  samples = load(f, Loader=SafeLoader)
 
 def _test(test, actual):
   isPassing = test['expected'] == actual
@@ -45,16 +49,10 @@ def _test(test, actual):
     testData = testData if not isPassing else ''
   )
 
-with open('2024-11-samples.yaml') as f:
-  samples = load(f, Loader=SafeLoader)
-
-for test in samples['stones']:
-  actual = blink(buildStones(test['input']), test['blinks'])
-  print(_test(test, ' '.join(map(str, actual))))
-
-for test in samples['count']:
+for test in samples:
   actual = main(test['input'], test['blinks'])
   print(_test(test, actual))
+
 
 with open('2024-11-input.txt') as f:
   challengeInput = f.read().strip()
