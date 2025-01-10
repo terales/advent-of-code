c97b431e65c980aa314435289ca25544f22b8fe8
Alex Terehov <1920639+terales@users.noreply.github.com>
2025-01-10 17:05:55 -0330

    zig day1: sample works

--- 2024-zig/src/day_01.zig
+++ 2024-zig/src/day_01.zig
@@ -14,7 +14,39 @@ pub fn main() !void {
 
     var splitSequence = std.mem.tokenizeAny(u8, content, " \n");
 
+    var leftList = std.ArrayList(u8).init(allocator);
+    defer leftList.deinit();
+    var rightList = std.ArrayList(u8).init(allocator);
+    defer rightList.deinit();
+    var rightListOccurrences = std.AutoHashMap(u8, u8).init(allocator);
+    defer rightListOccurrences.deinit();
+
+    var isEven = true;
+
     while (splitSequence.next()) |token| {
-        std.debug.print("{s}\n", .{token});
+        const tokenInt = try std.fmt.parseInt(u8, token, 10);
+
+        if (isEven) {
+            try leftList.append(tokenInt);
+        } else {
+            try rightList.append(tokenInt);
+
+            const occurrences = rightListOccurrences.get(tokenInt) orelse 0;
+            try rightListOccurrences.put(tokenInt, occurrences + 1);
+        }
+        isEven = !isEven;
     }
+
+    std.mem.sort(u8, leftList.items, {}, std.sort.asc(u8));
+    std.mem.sort(u8, rightList.items, {}, std.sort.asc(u8));
+
+    var totalDistance: u16 = 0;
+    var similarityScore: u16 = 0;
+    for (leftList.items, 0..) |leftItem, i| {
+        totalDistance += @abs(@as(i16, leftItem) - @as(i16, rightList.items[i]));
+        similarityScore += leftItem * (rightListOccurrences.get(leftItem) orelse 0);
+    }
+
+    std.debug.print("Total distance: {d}\n", .{totalDistance});
+    std.debug.print("Similarity score: {d}\n", .{similarityScore});
 }
