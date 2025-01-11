73ebf3ca733251b1b2336d2e2b83a7db4fa4f077
Alex Terehov <1920639+terales@users.noreply.github.com>
2025-01-11 09:16:55 -0330

    zig d2p2: solved

--- 2024-zig/src/day_02.zig
+++ 2024-zig/src/day_02.zig
@@ -7,40 +7,98 @@ pub fn main() !void {
     defer arena.deinit();
 
     var safeReports: u16 = 0;
+    var safeReportsWithProblemDampener: u16 = 0;
 
     const content = try aoc_utils.getInputContent(allocator);
     var rawReports = std.mem.tokenizeScalar(u8, content, '\n');
 
     while (rawReports.next()) |rawReport| {
+        var report = std.ArrayList(i16).init(allocator);
+
         var rawLevels = std.mem.tokenizeScalar(u8, rawReport, ' ');
-        var previousLevel = try std.fmt.parseUnsigned(i16, rawLevels.next() orelse "0", 10);
-
-        var levelIncreased = false;
-        var levelDecreased = false;
-
         while (rawLevels.next()) |rawLevel| {
             const level = try std.fmt.parseUnsigned(i16, rawLevel, 10);
+            try report.append(level);
+        }
 
-            const levelChange = level - previousLevel;
-            previousLevel = level;
-
-            if (@abs(levelChange) < 1 or @abs(levelChange) > 3) {
-                break;
-            }
-
-            if (levelChange > 0) {
-                levelIncreased = true;
-            } else if (levelChange < 0) {
-                levelDecreased = true;
-            }
-
-            if (levelIncreased and levelDecreased) {
-                break;
-            }
-        } else {
+        const analysis = analyzeReport(report);
+        if (analysis.isCorrect) {
             safeReports = safeReports + 1;
+            safeReportsWithProblemDampener = safeReportsWithProblemDampener + 1;
+            continue;
+        }
+
+        const hasProblemDampenerHelped = try doesProblemDampenerHelp(allocator, report, analysis.firstBadLevelIndex);
+        if (hasProblemDampenerHelped) {
+            safeReportsWithProblemDampener = safeReportsWithProblemDampener + 1;
         }
     }
 
     std.debug.print("Safe reports: {d}\n", .{safeReports});
+    std.debug.print("Safe reports thanks to the Problem Dampener: {d}\n", .{safeReportsWithProblemDampener});
+}
+
+const ReportAnalysis = struct {
+    isCorrect: bool,
+    firstBadLevelIndex: usize,
+};
+
+fn analyzeReport(report: std.ArrayList(i16)) ReportAnalysis {
+    var previousLevel = report.items[0];
+    var levelIncreased = false;
+    var levelDecreased = false;
+
+    for (report.items[1..], 0..) |level, index| {
+        const levelChange = level - previousLevel;
+        previousLevel = level;
+
+        if (@abs(levelChange) < 1 or @abs(levelChange) > 3) {
+            return ReportAnalysis{
+                .isCorrect = false,
+                .firstBadLevelIndex = index,
+            };
+        }
+
+        if (levelChange > 0) {
+            levelIncreased = true;
+        } else if (levelChange < 0) {
+            levelDecreased = true;
+        }
+
+        if (levelIncreased and levelDecreased) {
+            return ReportAnalysis{
+                .isCorrect = false,
+                .firstBadLevelIndex = index,
+            };
+        }
+    }
+
+    return ReportAnalysis{
+        .isCorrect = true,
+        .firstBadLevelIndex = 0,
+    };
+}
+
+fn doesProblemDampenerHelp(allocator: std.mem.Allocator, report: std.ArrayList(i16), badLevelIndex: usize) !bool {
+    const levelsToTryDumping = [3]usize{
+        badLevelIndex,
+        badLevelIndex + 1,
+        badLevelIndex -| 1,
+    };
+
+    for (levelsToTryDumping) |indexToDump| {
+        const dumpedReportSlice = try std.mem.concat(allocator, i16, &[_][]i16{
+            report.items[0..indexToDump],
+            report.items[indexToDump + 1 ..],
+        });
+
+        const dumpedReport = std.ArrayList(i16).fromOwnedSlice(allocator, dumpedReportSlice);
+
+        const dumpedReportAnalysis = analyzeReport(dumpedReport);
+        if (dumpedReportAnalysis.isCorrect) {
+            return true;
+        }
+    }
+
+    return false;
 }
