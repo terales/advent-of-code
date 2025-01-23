4c548800b243d50b1775f7a70133147d4de1e4a8
Alex Terehov <1920639+terales@users.noreply.github.com>
2025-01-22 21:09:55 -0330

    zig d3: sample works

---
+++ 2024-zig/src/day_03.zig
@@ -0,0 +1,33 @@
+const std = @import("std");
+const aoc_utils = @import("aoc_utils_lib");
+
+pub fn main() !void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    const allocator = arena.allocator();
+    defer arena.deinit();
+
+    const content = try aoc_utils.getInputContent(allocator);
+    var sum: u16 = 0;
+
+    var functions = std.mem.tokenizeSequence(u8, content, "mul(");
+    while (functions.next()) |function| {
+        var paramsIterator = std.mem.tokenizeScalar(u8, function, ')');
+        const params = paramsIterator.next();
+        if (params == null) {
+            continue;
+        }
+
+        var factors = std.mem.tokenizeScalar(u8, params orelse "", ',');
+        const multiplier = std.fmt.parseUnsigned(u16, factors.next() orelse "", 10) catch |err| switch (err) {
+            error.InvalidCharacter => continue,
+            else => return err,
+        };
+        const multiplicand = std.fmt.parseUnsigned(u16, factors.next() orelse "", 10) catch |err| switch (err) {
+            error.InvalidCharacter => continue,
+            else => return err,
+        };
+
+        sum = sum + multiplier * multiplicand;
+    }
+    std.debug.print("Part 1: {d}\n", .{sum});
+}
